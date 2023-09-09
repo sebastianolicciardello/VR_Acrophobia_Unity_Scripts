633594d370ee3a5f1786da02177438ecaaeefa78
sebastianolicciardello <80095600+sebastianolicciardello@users.noreply.github.com>
2023-09-09 22:38:48 +0200

    initialize login manager

--- LoginManager.cs
+++ LoginManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_InputField emailInputField, passwordInputField;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnLoginClick(){
+        string username = emailInputField.text;
+        string password = passwordInputField.text;
+
+        Debug.Log($"{username} : {password}");
     }
 }
